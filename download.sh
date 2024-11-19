36d1a91d65808e14b8949efb4b8551f86533b559
SeongJong-Yoo <ysj94755@naver.com>
2024-11-19 13:08:27 -0500

    Update download shell file

---
+++ download.sh
@@ -0,0 +1,23 @@
+cd ./config
+gdown 1K4ELUhZreCQduoBNAhQF5KMHAdrKMzTz
+
+mkdir ./Logs
+cd ./Logs
+gdown 1_8Z5iji5C0OBizMK571F24dGl0-XrJ6M
+gdown 10vxFQyFAiuEq4B0aeTQQwYquwKZwgsBj
+unzip ./VioPose.zip
+unzip ./VioPose_wo_audio.zip
+rm ./VioPose_wo_audio.zip
+rm ./VioPose.zip
+cd ..
+
+mkdir ./data
+mkdir ./data/demo
+mkdir ./data/FullData
+cd data/demo
+gdown 1OkjDUp2XRVWxOLAOIu5tZiYyO8RhJNmW
+gdown 1e8RzQjjfcoITnGQ5TbAQsvgkLD_H12sc
+cd ..
+
+mkdir ./output
+mkdir ./out_log
